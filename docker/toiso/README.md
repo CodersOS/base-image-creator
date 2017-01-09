ae94df778f539504bb93d2cc1fd047a9f2fe54d6
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-01-09 02:06:19 -0800

    added script to minimize the installation

---
+++ docker/toiso/README.md
@@ -0,0 +1,12 @@
+toiso
+=====
+
+
+
+
+`installed-minimal-packages.txt` is created by running
+
+    apt list --installed | grep -oE '^[^/]+'
+
+on ubuntu server.
+
---
+++ docker/toiso/make_minimal.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+cd "`dirname \"$0\"`"
+source "configuration.sh"
+
+minimal_packages="`cat installed-minimal-packages.txt | grep -vE '^\s*#|^\s*$'`"
+
+for package in $minimal_packages
+do
+  echo -n "Installing minimal package $package ... "
+  if true || apt-get -y -q --purge install "$package"
+  then
+    echo "ok"
+  else
+    echo "fail"
+  fi
+done
+
+for package in `apt list --installed | grep -oE '^[^/]+'`
+do
+  if apt-cache rdepends --recurse "$package" | grep -oE '\S+' | grep -qxF "$minimal_packages"
+  then
+    dependencies="`apt-cache rdepends --recurse \"$package\" | grep -oE '\S+' | grep -m 5 -oxF \"$minimal_packages\" | sort | uniq`"
+    echo "Not removing because of dependencies: $package ->" $dependencies
+  else
+    echo -n "Removing minimal package $package ... "
+    if true || apt-get -y -qq --purge remove "$package"
+    then
+      echo "ok"
+    else
+      echo "fail"
+    fi
+  fi
+done
